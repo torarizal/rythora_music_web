06b7173a4dad80fc60aabbd7fe01a25f85c2a47f
Tora Rizal Pratama <torarizal12@gmail.com>
2025-12-01 21:58:38 +0700

    fix bug

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1884000875"
+    serviceWorkerVersion: "4124728755"
   }
 });
